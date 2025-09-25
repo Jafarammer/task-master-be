0087992d7ee2fd34fc6dbd72ed9d9777a3d073e9
Your Name <jafalkaff96@gmail.com>
2025-09-25 09:08:06 +0700

    add list cors

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(helmet());
 app.use(morgan("dev"));
 
