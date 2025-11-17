ec846b9f6daa2fb21d9026c97a6da2a56a169198
Your Name <jafalkaff96@gmail.com>
2025-11-17 08:40:05 +0700

    enhance fungsi task service & controller

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -2,41 +2,19 @@ import { Request, Response } from "express";
 import * as taskService from "../services/task.service";
 
 export const handleCreateTask = async (req: Request, res: Response) => {
-  try {
-    const user_id = (req as any).user?.id;
+  const user_id = (req as any).user?.id;
+  const { title, description, due_date, priority } = req.body;
 
-    const { title, description, due_date, priority } = req.body;
+  const task = await taskService.createTask(
+    user_id,
+    title,
+    description,
+    due_date,
+    priority
+  );
 
-    if (!user_id || !title || !description || !due_date) {
-      return res.status(404).json({
-        message: "user_id, title, description,dan due_date is required.",
-      });
-    }
-
-    const allowed: string[] = ["low", "medium", "high"];
-    const prio: string = priority ?? "medium";
-
-    if (!allowed.includes(prio)) {
-      return res
-        .status(400)
-        .json({ message: `priority must be one of ${allowed.join(",")}` });
-    }
-
-    const newTask = await taskService.createTask(
-      user_id,
-      title,
-      description,
-      due_date,
-      prio
-    );
-
-    return res.status(201).json({
-      data: newTask,
-      message: "Create task success",
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error?.message ?? "Internal server error",
-    });
+  if (task.error) {
+    return res.status(task.code).json({ message: task.message });
   }
+  return res.status(201).json({ data: task, message: "Create task success" });
 };
--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,24 +1,54 @@
 import Task, { ITask } from "../models/task.model";
 import { Types } from "mongoose";
 
-export const createTask = (
+interface IServiceResult {
+  error?: boolean;
+  code?: number;
+  message?: string;
+  data?: ITask | null;
+}
+
+export const createTask = async (
   user_id: Types.ObjectId | string,
   title: string,
   description: string,
   due_date: Date | string,
   priority: string
-): Promise<ITask> => {
-  const userObjectId =
-    typeof user_id === "string" ? new Types.ObjectId(user_id) : user_id;
+): Promise<IServiceResult> => {
+  try {
+    if (!user_id || !title || !description || !due_date) {
+      return {
+        error: true,
+        code: 404,
+        message: "user_id, title, description,dan due_date is required.",
+      };
+    }
+    const allowed: string[] = ["low", "medium", "high"];
+    const prio: string = priority ?? "medium";
+    if (!allowed.includes(prio)) {
+      return {
+        error: true,
+        code: 400,
+        message: `priority must be one of ${allowed.join(",")}`,
+      };
+    }
 
-  const dueDateObj =
-    typeof due_date === "string" ? new Date(due_date) : due_date;
+    const userObjectId =
+      typeof user_id === "string" ? new Types.ObjectId(user_id) : user_id;
 
-  return Task.create({
-    user_id: userObjectId,
-    title,
-    description,
-    due_date: dueDateObj,
-    priority,
-  });
+    const dueDateObj =
+      typeof due_date === "string" ? new Date(due_date) : due_date;
+
+    const newTask = await Task.create({
+      user_id: userObjectId,
+      title,
+      description,
+      due_date: dueDateObj,
+      priority: prio,
+    });
+
+    return { data: newTask };
+  } catch (error: any) {
+    return { error: true, code: 500, message: "Internal server error" };
+  }
 };
