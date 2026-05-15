c473694497b7abaa88b9280e8a12b99376bbeec1
Your Name <jafalkaff96@gmail.com>
2026-05-15 08:16:23 +0700

    config env cloudinary

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -18,4 +18,7 @@ export const CLIENT_HOST: string =
   process.env.CLIENT_HOST || "http://localhost:5173";
 export const VERIFICATION_HOST =
   process.env.VERIFICATION_HOST || "http://localhost:8000";
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
 export const NODE_ENV = process.env.NODE_ENV || "development";
