a0e58063ffedf2fa7b0915c89552b6df6cb6c50b
Your Name <jafalkaff96@gmail.com>
2026-05-23 08:14:18 +0700

    enhance end point get task completed

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -148,29 +148,24 @@ export const handleGetTaskCompleted = async (
   req: AuthRequest,
   res: Response,
 ) => {
-  const user_id: string = req.user?.id;
-  const page: number = Number(req.query.page) || 1;
-  const limit: number = Number(req.query.limit) || 5;
-  const sort_by: string = String(req.query.sort_by) || "createdAt";
-  const order: "asc" | "desc" = req.query.order === "asc" ? "asc" : "desc";
-  const query: string = String(req.query.search) || "";
+  const userId = req.user.id;
+  const params: IServiceParams = {
+    page: Number(req.query.page) || 1,
+    limit: Number(req.query.limit) || 5,
+    sortBy: (req.query.sortBy as string) || "createdAt",
+    order: (req.query.order as "asc" | "desc") || "desc",
+    query: (req.query.query as string) || "",
+  };
 
-  const result = await taskService.getTaskCompleted({
-    user_id,
-    page,
-    limit,
-    sort_by,
-    order,
-    query,
-  });
+  const result = await taskService.getTaskCompleted(userId, params);
 
   if (result.error) {
     return res.status(result.code).json({ message: result.message });
   }
 
   return res
-    .status(201)
-    .json({ data: result.data, meta_data: result.pagination });
+    .status(result.code)
+    .json({ data: result.data.tasks, meta_data: result.data.pagination });
 };
 
 export const handleGetTaskPending = async (req: AuthRequest, res: Response) => {
