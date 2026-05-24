c4cfdb250f383bb17c3e2d8201a06b22e6ff9622
Your Name <jafalkaff96@gmail.com>
2026-05-25 04:36:04 +0700

    chane validation update task

--- src/validations/task.validate.ts
+++ src/validations/task.validate.ts
@@ -14,9 +14,8 @@ export const createTaskValidation = z.object({
 export const updateTaskValidation = z.object({
   title: z.string({ error: "Title is required" }),
   description: z.string({ error: "Description is required" }),
-  dueDate: z.coerce
-    .date({ error: "Due date is required" })
-    .min(new Date().setHours(0, 0, 0, 0), "Date cannot be earlier than today"),
+  dueDate: z.coerce.date({ error: "Due date is required" }),
+  // .min(new Date().setHours(0, 0, 0, 0), "Date cannot be earlier than today"),
   priority: z.enum(["low", "medium", "high"], {
     error: "priority must be one of, low, medium, high ",
   }),
