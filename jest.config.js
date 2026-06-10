378181dcd49700571d6c18fb630dd206608bbb62
Your Name <jafalkaff96@gmail.com>
2026-06-10 18:04:07 +0700

    setup jest testings

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
---
+++ tests/auth.test.ts
@@ -0,0 +1,5 @@
+describe("First Test", () => {
+  it("should pass", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
